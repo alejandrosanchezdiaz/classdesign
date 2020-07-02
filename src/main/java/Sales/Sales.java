da86be57d488eea5b3cf625dc23dfe4e0bc9a315
Alex <alesandi.uk@gmail.com>
2020-07-02 17:01:39 +0100

    5-commit
    Import Product Class

--- src/main/java/Sales/Sales.java
+++ src/main/java/Sales/Sales.java
@@ -1,5 +1,10 @@
 package Sales;
 
+import com.gm.sales.Product;
+
 public class Sales {
-    
+    public static void main(String[] args) {
+        //Let's create a buch of product type objects
+        Product product1 = new Product("Shirt", 50);
+    }
 }
